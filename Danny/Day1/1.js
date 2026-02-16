77ad4b1b8f77b6c7b6fff9b2be2d45b620da87aa
Wells Wait <wells.wait.coder@gmail.com>
2026-02-16 14:14:25 -0700

    first progress file

--- Danny/Day1/1.js
+++ Danny/Day1/1.js
@@ -3,11 +3,27 @@
 
 
 
-function PULL(){
-    const button = document.getElementById('inPUT');
+
+async function PULL() {
+    const button = document.getElementById('inPut');
+    
+    const url = `https://github.com/Wells-Wait/EscapeTheRoom/Danny.ESCAPE`;
+
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            // This is the "ID/Key" that lets you in
+            "Authorization": `Bearer ${GITHUB_TOKEN}`, 
+            "Accept": "application/vnd.github.v3+json"
+        }
+    });
+
+    const data = await response.json();
+    
+    // Remember: data.content is the encoded text
+    // data.sha is the "ID" you need later to save!
+
     button.innerHTML = "complete";
-
-
-
-
+    button.disabled = true;
+    return data; 
 }
